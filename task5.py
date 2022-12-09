53306d44039130ce7b39fc256b3f815e37f5e222
Elena Gerasimovich <nararock@mail.ru>
2022-12-09 13:34:24 +0300

    task5 fixed

--- task5.py
+++ task5.py
@@ -5,7 +5,3 @@ with open("task5.txt", 'w', encoding="utf-8") as f_obj:
 with open("task5.txt", 'r', encoding="utf-8") as f_obj:
     my = list(float(el) for el in (f_obj.read().split(' ')))
     print(sum(my))
-
-
-
-
